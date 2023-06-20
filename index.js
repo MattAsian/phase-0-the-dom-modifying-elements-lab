54a688287dba2fd87cd0574e27827e2a96b075ea
Matthew Yang <matthewyang434@gmail.com>
2023-06-19 22:39:56 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement('h1')
+const element = document.getElementById("main")
+element.remove()
+newHeader.id = "victory"
+newHeader.textContent = "MattAsian is the champion"
